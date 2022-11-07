1590cf6c50f9f4f76fff7e1482f129133324478a
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2022-11-07 12:49:18 -0300

    feat(screen): creating SignIn screen

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,34 @@
+import { Center, Box, Flex, Text } from "native-base";
+
+import Logo from "@/assets/logo.svg";
+import GoogleIcon from "@/assets/icons/google.svg";
+
+import { Button } from "@/components/Button";
+import { useAuth } from "@/hooks/useAuth";
+
+export function SignIn() {
+  const { signIn, user } = useAuth();
+
+  console.log(user);
+
+  return (
+    <Center flex={1} bg="gray.900">
+      <Flex w="4/5" alignItems="center" mt="8">
+        <Box mb="12">
+          <Logo width={212} height={40} />
+        </Box>
+
+        <Button
+          title="Continuar com o google"
+          rounded={"3xl"}
+          leftIcon={<GoogleIcon height={32} />}
+        />
+
+        <Text color="gray.300" lineHeight="lg" mt="7" textAlign="center">
+          Não utilizamos nenhuma informação além {"\n"}
+          do seu e-mail para criação de sua conta.
+        </Text>
+      </Flex>
+    </Center>
+  );
+}
